2d90a605052e980e86684b82e731cb451ef606d4
Cemil <okcuoglucemil@gmail.com>
2023-02-01 15:59:46 +0300

    Iterators

--- src/main/java/day34mapsiterators/Iterators01.java
+++ src/main/java/day34mapsiterators/Iterators01.java
@@ -1,4 +1,36 @@
 package day34mapsiterators;
 
 public class Iterators01 {
+
+    /*
+
+    Iterator'lar Loop'larin yaptığı işi yapar.
+    Iterator'lar tıpkı loop'larda olduğu gibi elemanı alarak işleme sokar.
+    Java'nın Iterator'lari ortaya cıkarma sebebi Infinite loop riskini bitirmektir.
+    Iterator'larda sonsuz loop yoktur.
+    Iterator'lar sonradan oluşturmuşlardır ve loopların yaptıkları bütün işlemleri yapabilirler.
+    Loop'lar ile Iterator'larin performans farkı yoktur ama Iterator'lar eleman silme ve update etmede daha basarılıdırlar.
+    Collection'larda eleman silme ya da update islemlerinde Iterator kullanırız.
+
+    Iki tip Iterator vardır;
+    i) Iterator: Sadece elemanlari remove edebilirsiniz.
+
+
+
+     */
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
