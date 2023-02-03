f981c8661941d5fc316cc57dc3249f585532377f
Cemil <okcuoglucemil@gmail.com>
2023-02-03 18:21:17 +0300

    practice

---
+++ src/main/java/day35lambda/Lambda01Practice.java
@@ -0,0 +1,52 @@
+package day35lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lambda01Practice {
+
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(12);
+        nums.add(9);
+        nums.add(131);
+        nums.add(14);
+        nums.add(9);
+        nums.add(10);
+        nums.add(4);
+        nums.add(12);
+        nums.add(15);
+
+        printElements(nums);
+        System.out.println();
+        printEvenElements2(nums);
+        System.out.println();
+        printSquareOfOddElements(nums);
+
+
+    }
+
+    public static void printElements(List<Integer> nums){
+
+        nums.stream().forEach(t -> System.out.print(t + " "));
+    }
+
+    public static void printEvenElements2(List<Integer> nums){
+
+        nums.
+                stream().
+                filter(t->t%2==0).
+                forEach(t-> System.out.print(t + " "));
+    }
+    public static void printSquareOfOddElements(List<Integer> nums){
+
+        nums.
+                stream().
+                filter(t->t%2!=0).
+                map(t->t*t).
+                forEach(t-> System.out.print(t + " "));
+    }
+
+
+}
