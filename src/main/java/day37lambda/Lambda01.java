e2efe9f9700aba3f93a5a9456149c150e9261615
Cemil <okcuoglucemil@gmail.com>
2023-02-04 22:14:44 +0300

    lambda2

--- src/main/java/day37lambda/Lambda01.java
+++ src/main/java/day37lambda/Lambda01.java
@@ -2,6 +2,7 @@ package day37lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Lambda01 {
 
@@ -15,9 +16,59 @@ public class Lambda01 {
         myList.add(10);
         myList.add(7);
 
-
+        System.out.println(getSumOfAllElements(myList));
+        System.out.println(getSumFromSevenToSeventy());
+        System.out.println(getMultiplicationFromThreeToNine());
+        System.out.println(getMultiplicationFromThreeToNine2());
+        System.out.println(getFactorial(5));
 
     }
+
+    //Example 1: Verilen list'teki tum sayilarin toplamini veren method'u olusturunuz.
+
+    public static int getSumOfAllElements(List<Integer> myList){
+
+        //Math::addExact :Matematik Class'ina git, tamamını topla.
+
+       return myList.stream().reduce(Math::addExact).get();
+
+    }
+
+    //Example 2: 7'den 70'e kadar tum tamsayilarin toplamini veren kodu yaziniz.
+
+    public static int getSumFromSevenToSeventy(){
+
+        //IntStream integerları yukardan asagıya dizer.
+        //rangeClosed==> baslangis ve bitis dahil olarak size int aralıgı veriyor.
+      return IntStream.rangeClosed(7,70).reduce(Math::addExact).getAsInt();
+
+    }
+
+    //Example 3: 3'den 9' a kadar tum tamsayilarin carpimini veren methodu olusturunuz.
+    public static int getMultiplicationFromThreeToNine(){
+        return IntStream.rangeClosed(3,9).reduce(Math::multiplyExact).getAsInt();
+    }
+
+
+    public static int getMultiplicationFromThreeToNine2(){
+
+        //range methodu ilki dahil sonuncuyu dahil olarak vermez o yüzden 1 fazlası.
+        return IntStream.range(3,10).reduce(Math::multiplyExact).getAsInt();
+    }
+
+    //4) Size verilen sayinin faktoriyelini hesaplayan kodu yaziniz.
+    public static int getFactorial(int x){
+        if(x==0){
+            return 1; //0 ın faktöriyeli 1 dir.
+        }else if(x<0){
+            System.out.println("Faktoriyel islemi negatif sayılarla yapılamaz");
+            return -1; //hata mesajı olarak java -1 verdigi icin biz de -1 yazdık.
+        }
+        return IntStream.rangeClosed(1,x).reduce(Math::multiplyExact).getAsInt();
+
+    }
+
+
 
 
 
---
+++ src/main/java/lambdapractice/Utils.java
@@ -0,0 +1,16 @@
+package lambdapractice;
+
+public class Utils {
+
+    public static void yazdir(Object a){ //Object data tipi olunca String, int vb. her turlu elemani kullanabiliriz
+        System.out.print(a + " ");
+    }//parametreyi object yaptik, int, String vs. hepsinde kullanabilmek icin
+
+    public static int kareBul(int a){
+        return a*a;
+    }
+
+
+
+
+}
