589e1e87dc27d716f4db05c20e4ba19f720f2996
Cemil <okcuoglucemil@gmail.com>
2023-02-27 12:55:22 +0300

    general commit

---
+++ src/main/java/lambdapractice/lambdaobjectpractice/Apartman.java
@@ -0,0 +1,25 @@
+package lambdapractice.lambdaobjectpractice;
+
+public class Apartman {
+
+  private  String cephe;
+
+  private  int katSayi;
+
+  private  int kira;
+
+    public Apartman() {
+    }
+
+  public Apartman(String cephe, int katSayi, int kira) {
+    this.cephe = cephe;
+    this.katSayi = katSayi;
+    this.kira = kira;
+  }
+
+
+
+
+
+
+}
